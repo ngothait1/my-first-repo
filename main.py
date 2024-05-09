6c8498e2fb2b16333cc87bbae98d3decffce71e1
Nadav Gothait <nadavgot@gmail.com>
2024-05-09 16:24:34 +0300

    Adding print from programmer 1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Student import Student
 from Employee import Employee
 
 student = Student("Mike", 22, "Engineering", 1, 70)
+print("This is from programmer 1")
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
